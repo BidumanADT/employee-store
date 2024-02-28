6c7ac42a5efc0c0428824f937209e5894bf1e7d9
Aaron D. Tanner <sgtadt@gmail.com>
2024-02-28 16:04:47 -0600

    added a welcome section to top of page for displaying text. future development could inclue an image carousel of featured items.

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -117,81 +117,104 @@ const ProductListing = () => {
 
   return (
     <div className={styles.mainContent}>
-      <div className={styles.filterSidebar}>
-        <h5 className={styles.filterHeader}>Category</h5>
-        {categories.map(category => (
-          <div key={category} className={styles.filterOption}>
-            <input
-              type="checkbox"
-              id={category}
-              name={category}
-              value={category}
-              className={styles.filterCheckbox}
-              onChange={e => handleCategoryChange(category, e.target.checked)}
-              checked={selectedCategories.includes(category)}
-            />
-            <label htmlFor={category} className={styles.filterLabel}>
-              {category} ({categoryCounts[category] || 0})
-            </label>
-          </div>
-        ))}
-        <button className={styles.applyButton} onClick={() => applyFilters()}>
-          Apply Filters
-        </button>
-        <button className={styles.clearButton} onClick={clearFilters}>
-          Clear All
-        </button>
+      {/* Welcome section */}
+      <div className={styles.welcomeSection}>
+        <h2>Welcome to the Company Store!</h2>
+        <h4>Explore our latest products and offers.</h4>
+        <p>
+          Qui irure amet amet laboris sint anim aliquip consectetur sint ipsum
+          aliquip minim Lorem do. Excepteur est nisi adipisicing consectetur
+          adipisicing fugiat cupidatat officia eu culpa pariatur do labore
+          ipsum. Aliqua labore adipisicing fugiat aliquip labore aliqua veniam
+          incididunt sit amet. Voluptate eiusmod qui ullamco velit aliqua ipsum
+          reprehenderit. Nostrud adipisicing anim tempor sint et dolor sit nulla
+          pariatur sunt irure. Consequat proident excepteur fugiat cupidatat.
+          Nisi anim culpa sint officia incididunt.
+        </p>
       </div>
-      <Container className={styles.productContainer}>
-        <Row>
-          {filteredProducts.map(({ node }) => (
-            <Col xs={12} sm={6} md={4} lg={3} key={node.id}>
-              <Card style={{ width: "18rem", margin: "10px" }}>
-                <Card.Img
-                  variant="top"
-                  src={node.Image?.publicURL || "./data/images/default.jpeg"}
-                  alt={node.OriginalName || "Default Image"}
-                  style={{ height: "200px", objectFit: "cover" }} // Ensures images are the same size
-                />
-                <Card.Body style={{ minHeight: "210px" }}>
-                  {" "}
-                  {/* Adjust minHeight as needed */}
-                  <Card.Title>
-                    {node.NewName ? node.NewName : node.OriginalName}
-                  </Card.Title>
-                  <Card.Text>
-                    {node.Description && node.Description.substring(0, 100)}
-                    {node.Description && node.Description.length > 100
-                      ? "..."
-                      : ""}
-                  </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                  <ListGroup.Item>
-                    {node.OneSize
-                      ? "One Size Fits Most"
-                      : "Multiple Sizes Available"}
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    Price:{" "}
-                    {node.OneSize
-                      ? `${new Intl.NumberFormat("en-US", {
-                          style: "currency",
-                          currency: "USD",
-                        }).format(node._1SizePrice)}`
-                      : "Varies"}
-                  </ListGroup.Item>
-                </ListGroup>
-                <Card.Body>
-                  <Card.Link href="#" onClick={e => handleShowDetail(node, e)}>
-                    Show Details
-                  </Card.Link>
-                </Card.Body>
-              </Card>
-            </Col>
+      {/* Sidebar and Listing sections */}
+      <div className={styles.sidebarAndListing}>
+        <div className={styles.filterSidebar}>
+          {/* Category filter UI */}
+          <h5 className={styles.filterHeader}>Category</h5>
+          {categories.map(category => (
+            <div key={category} className={styles.filterOption}>
+              <input
+                type="checkbox"
+                id={category}
+                name={category}
+                value={category}
+                className={styles.filterCheckbox}
+                onChange={e => handleCategoryChange(category, e.target.checked)}
+                checked={selectedCategories.includes(category)}
+              />
+              <label htmlFor={category} className={styles.filterLabel}>
+                {category} ({categoryCounts[category] || 0})
+              </label>
+            </div>
           ))}
-        </Row>
-      </Container>
+          <button className={styles.applyButton} onClick={() => applyFilters()}>
+            Apply Filters
+          </button>
+          <button className={styles.clearButton} onClick={clearFilters}>
+            Clear All
+          </button>
+        </div>
+        {/* Product listing section */}
+        <Container className={styles.productContainer}>
+          <Row>
+            {filteredProducts.map(({ node }) => (
+              <Col xs={12} sm={6} md={4} lg={3} key={node.id}>
+                <Card style={{ width: "18rem", margin: "10px" }}>
+                  <Card.Img
+                    variant="top"
+                    src={node.Image?.publicURL || "./data/images/default.jpeg"}
+                    alt={node.OriginalName || "Default Image"}
+                    style={{ height: "200px", objectFit: "cover" }} // Ensures images are the same size
+                  />
+                  <Card.Body style={{ minHeight: "210px" }}>
+                    {" "}
+                    {/* Adjust minHeight as needed */}
+                    <Card.Title>
+                      {node.NewName ? node.NewName : node.OriginalName}
+                    </Card.Title>
+                    <Card.Text>
+                      {node.Description && node.Description.substring(0, 100)}
+                      {node.Description && node.Description.length > 100
+                        ? "..."
+                        : ""}
+                    </Card.Text>
+                  </Card.Body>
+                  <ListGroup className="list-group-flush">
+                    <ListGroup.Item>
+                      {node.OneSize
+                        ? "One Size Fits Most"
+                        : "Multiple Sizes Available"}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      Price:{" "}
+                      {node.OneSize
+                        ? `${new Intl.NumberFormat("en-US", {
+                            style: "currency",
+                            currency: "USD",
+                          }).format(node._1SizePrice)}`
+                        : "Varies"}
+                    </ListGroup.Item>
+                  </ListGroup>
+                  <Card.Body>
+                    <Card.Link
+                      href="#"
+                      onClick={e => handleShowDetail(node, e)}
+                    >
+                      Show Details
+                    </Card.Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </div>
       {/* Product Detail Modal */}
       <ProductDetail
         product={selectedProduct}
