8d63c5de08625c11364dd358699ae4ae40031ae9
Aaron D. Tanner <sgtadt@gmail.com>
2024-05-02 16:05:22 -0500

    removed redundant CartService.js file. added a live count badge to track how many items are in the cart.

--- src/components/CartIcon.js
+++ src/components/CartIcon.js
@@ -1,18 +1,30 @@
 import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Badge } from 'react-bootstrap';
 import { FaShoppingCart } from 'react-icons/fa';
 import CartModal from './CartModal';
 import * as styles from  './CartIcon.module.css';
+import { useCart } from './CartContext';
 
 const CartIcon = () => {
     const [showModal, setShowModal] = useState(false);
+    const { cart } = useCart(); // Access cart from the context
 
+    // Function to show/hide modal
     const toggleModal = () => setShowModal(!showModal);
+
+    // Function to calculate total number of items in the cart
+    const getItemCount = () => {
+        return cart.reduce((total, item) => total + item.quantity, 0);
+    };
 
     return (
         <>
             <Button className={styles.cartIcon} onClick={toggleModal}>
                 <FaShoppingCart size={24} />
+                {/* Display item count badge if there are items in the cart */}
+                {cart.length > 0 && (
+                    <Badge className={styles.badge}>{getItemCount()}</Badge>
+                )}
             </Button>
             <CartModal show={showModal} onHide={toggleModal} />
         </>
