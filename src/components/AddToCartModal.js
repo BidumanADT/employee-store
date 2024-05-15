5e82a50dd3d5dffd50237b4b9c22a8b90c401f54
Aaron D. Tanner <sgtadt@gmail.com>
2024-05-15 13:26:31 -0500

    added an image url as a prop so it can be used on the checkout page. added a small image of the product to the checkout page table. moving on to improving the table.

--- src/components/AddToCartModal.js
+++ src/components/AddToCartModal.js
@@ -1,66 +1,79 @@
-import React, { useState, useEffect } from "react";
-import { Modal, Button, Form, Container } from "react-bootstrap";
-import { useCart } from "./CartContext";
-import { formatCurrency } from "./utils";
+import React, { useState, useEffect } from "react"
+import { Modal, Button, Form, Container } from "react-bootstrap"
+import { useCart } from "./CartContext"
+import { formatCurrency } from "./utils"
 
 const AddToCartModal = ({ product, show, onHide }) => {
-  const { addToCart } = useCart();
-  const [selectedSize, setSelectedSize] = useState("");
-  const [quantity, setQuantity] = useState(1);
+  const { addToCart } = useCart()
+  const [selectedSize, setSelectedSize] = useState("")
+  const [quantity, setQuantity] = useState(1)
 
   // Effect to set the default selected size based on availability
   useEffect(() => {
     if (product) {
-      const sizes = ["Xs", "Sm", "Md", "Lg", "Xl", "_2x", "_3x", "_4x", "_6x"];
-      const availableSize = sizes.find(size => product[`${size}Inv`] > 0);
+      const sizes = ["Xs", "Sm", "Md", "Lg", "Xl", "_2x", "_3x", "_4x", "_6x"]
+      const availableSize = sizes.find(size => product[`${size}Inv`] > 0)
       if (availableSize) {
-        setSelectedSize(availableSize);  // Keep the original key format for operations
+        setSelectedSize(availableSize) // Keep the original key format for operations
       } else if (product.OneSize) {
-        setSelectedSize("OneSize");  // Use "OneSize" for one-size products
+        setSelectedSize("OneSize") // Use "OneSize" for one-size products
       }
     }
-  }, [product]);
+  }, [product])
 
   const handleAddToCart = () => {
-    const priceKey = selectedSize === "OneSize" ? "_1SizePrice" : `${selectedSize}Price`;
-    const price = product[priceKey];
+    const priceKey =
+      selectedSize === "OneSize" ? "_1SizePrice" : `${selectedSize}Price`
+    const price = product[priceKey]
+    const imageUrl = product.Image?.publicURL
 
-    console.log(`Selected Size: ${selectedSize}, Price Key: ${priceKey}, Price: ${price}`) // Debugging
+    console.log(
+      `Selected Size: ${selectedSize}, Price Key: ${priceKey}, Price: ${price}`
+    ) // Debugging
 
     if (price && quantity > 0) {
       addToCart({
         name: product.NewName || product.OriginalName,
         price,
-        size: selectedSize,  // Send the size key directly as stored in product data
+        size: selectedSize, // Send the size key directly as stored in product data
         quantity,
-      });
-      onHide(); // Close modal after adding to cart
+        image: imageUrl
+      })
+      onHide() // Close modal after adding to cart
     } else {
-      console.error("Failed to add to cart: Price not found or quantity invalid.");
+      console.error(
+        "Failed to add to cart: Price not found or quantity invalid."
+      )
     }
-  };
+  }
 
   // Render dropdown options with user-friendly format but use accurate data keys
   const renderSizeOptions = () => {
-    if (!product) return null;
+    if (!product) return null
 
-    const sizes = ["Xs", "Sm", "Md", "Lg", "Xl", "_2x", "_3x", "_4x", "_6x"];
+    const sizes = ["Xs", "Sm", "Md", "Lg", "Xl", "_2x", "_3x", "_4x", "_6x"]
     return product.OneSize ? (
-      <option value="OneSize">One Size - {formatCurrency(product._1SizePrice)}</option>
-    ) : sizes.map(size => {
-      const priceKey = `${size}Price`;
-      const inventoryKey = `${size}Inv`;
-      const price = product[priceKey];
-      const inventory = product[inventoryKey];
-      if (inventory > 0) {
-        const displaySize = size.replace('_', '').toUpperCase(); // Format for display
-        return (
-          <option key={size} value={size}>{`${displaySize} - ${formatCurrency(price)} - ${inventory} available`}</option>
-        );
-      }
-      return null;
-    });
-  };
+      <option value="OneSize">
+        One Size - {formatCurrency(product._1SizePrice)}
+      </option>
+    ) : (
+      sizes.map(size => {
+        const priceKey = `${size}Price`
+        const inventoryKey = `${size}Inv`
+        const price = product[priceKey]
+        const inventory = product[inventoryKey]
+        if (inventory > 0) {
+          const displaySize = size.replace("_", "").toUpperCase() // Format for display
+          return (
+            <option key={size} value={size}>{`${displaySize} - ${formatCurrency(
+              price
+            )} - ${inventory} available`}</option>
+          )
+        }
+        return null
+      })
+    )
+  }
 
   return (
     <Modal show={show} onHide={onHide} centered size="sm">
@@ -72,23 +85,36 @@ const AddToCartModal = ({ product, show, onHide }) => {
           <Form>
             <Form.Group controlId="productSize">
               <Form.Label>Size</Form.Label>
-              <Form.Control as="select" value={selectedSize} onChange={e => setSelectedSize(e.target.value)}>
+              <Form.Control
+                as="select"
+                value={selectedSize}
+                onChange={e => setSelectedSize(e.target.value)}
+              >
                 {renderSizeOptions()}
               </Form.Control>
             </Form.Group>
             <Form.Group controlId="productQuantity">
               <Form.Label>Quantity</Form.Label>
-              <Form.Control type="number" value={quantity} min="1" onChange={e => setQuantity(parseInt(e.target.value, 10))} />
+              <Form.Control
+                type="number"
+                value={quantity}
+                min="1"
+                onChange={e => setQuantity(parseInt(e.target.value, 10))}
+              />
             </Form.Group>
           </Form>
         </Container>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="primary" onClick={handleAddToCart}>Add to Cart</Button>
-        <Button variant="secondary" onClick={onHide}>Close</Button>
+        <Button variant="primary" onClick={handleAddToCart}>
+          Add to Cart
+        </Button>
+        <Button variant="secondary" onClick={onHide}>
+          Close
+        </Button>
       </Modal.Footer>
     </Modal>
-  );
-};
+  )
+}
 
-export default AddToCartModal;
+export default AddToCartModal
--- src/components/CheckoutPage.js
+++ src/components/CheckoutPage.js
@@ -1,74 +1,92 @@
-import React from 'react'
-import { useCart } from './CartContext'
-import { useAppContext } from './AppContext'
-import { Button, Table } from 'react-bootstrap'
-import * as styles from './CheckoutPage.module.css'
+import React from "react"
+import { useCart } from "./CartContext"
+import { useAppContext } from "./AppContext"
+import { Button, Table } from "react-bootstrap"
+import * as styles from "./CheckoutPage.module.css"
 
-const CheckoutPage = ({ onReturnToCart }) => {
-    const {cart, updateCartItem, removeCartItem } = useCart()
-    const { setIsCheckoutVisible } = useAppContext()
+const CheckoutPage = () => {
+  const { cart, updateCartItem, removeCartItem } = useCart()
+  const { setIsCheckoutVisible } = useAppContext()
 
-    // Functions to alter quantities
-    const incrementQuantity = (item) => {
-        if (item.quantity < 999) {
-            updateCartItem({ ...item, quantity: item.quantity + 1})
-        }
+  // Functions to alter quantities
+  const incrementQuantity = item => {
+    if (item.quantity < 999) {
+      updateCartItem({ ...item, quantity: item.quantity + 1 })
     }
-    const decrementQuantity = (item) => {
-        if (item.quantity > 1) {
-            updateCartItem({ ...item, quantity: item.quantity - 1})
-        }
+  }
+  const decrementQuantity = item => {
+    if (item.quantity > 1) {
+      updateCartItem({ ...item, quantity: item.quantity - 1 })
     }
+  }
 
-    // Function to remove item
-    const handleRemoveItem = (item) => {
-        removeCartItem(item.name, item.size)
-    }
+  // Function to remove item
+  const handleRemoveItem = item => {
+    removeCartItem(item.name, item.size)
+  }
 
-    // Function to handle navigating back to products page
-    const handleReturnToProducts = ()=> {
-        setIsCheckoutVisible(false)
-    }
+  // Function to handle navigating back to products page
+  const handleReturnToProducts = () => {
+    setIsCheckoutVisible(false)
+  }
 
-    return (
-        <div className={styles.checkoutPage}>
-            <h2>Checkout</h2>
-            <Table striped bordered hover>
-                <thread>
-                    <tr>
-                        <th>Product</th>
-                        <th>Size</th>
-                        <th>Quantity</th>
-                        <th>Price</th>
-                        <th>Total</th>
-                        <th>Remove</th>
-                    </tr>
-                </thread>
-                <tbody>
-                    {cart.map(item => (
-                        <tr key={`${item.name}-${item.size}`}>
-                            <td>{item.name}</td>
-                            <td>{item.size.replace('_', '').toUpperCase()}</td>
-                            <td>
-                                <Button variant='light' onClick={()=> decrementQuantity(item)}>-</Button>
-                                {' '}{item.quantity}{' '}
-                                <Button variant='light' onClick={()=> incrementQuantity(item)}>+</Button>
-                            </td>
-                            <td>${item.price.toFixed(2)}</td>
-                            <td>${(item.price * item.quantity).toFixed(2)}</td>
-                            <td>
-                                <Button variant='outline-danger' onClick={()=> handleRemoveItem(item)}>Remove</Button>
-                            </td>
-                        </tr>
-
-                    ))}
-                </tbody>
-            </Table>
-            <div className={styles.checkoutActions}>
-                <Button variant='outline-secondary' onClick={handleReturnToProducts}>Back to Products</Button>
-                <Button variant='outline-success'>Confirm Checkout</Button> {/* Placeholder for checkout functionality */}
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.checkoutPage}>
+      <h2>Checkout</h2>
+      <Table striped bordered hover>
+        <thread>
+          <tr>
+            <th>Product</th>
+            <th>Size</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Total</th>
+            <th>Remove</th>
+          </tr>
+        </thread>
+        <tbody>
+          {cart.map(item => (
+            <tr key={`${item.name}-${item.size}`}>
+              <td>
+                <img
+                  className={styles.thumbImage}
+                  src={item.image}
+                  alt={item.OriginalName || "Product Image"}
+                />
+                {item.name}
+              </td>
+              <td>{item.size.replace("_", "").toUpperCase()}</td>
+              <td>
+                <Button variant="light" onClick={() => decrementQuantity(item)}>
+                  -
+                </Button>{" "}
+                {item.quantity}{" "}
+                <Button variant="light" onClick={() => incrementQuantity(item)}>
+                  +
+                </Button>
+              </td>
+              <td>${item.price.toFixed(2)}</td>
+              <td>${(item.price * item.quantity).toFixed(2)}</td>
+              <td>
+                <Button
+                  variant="outline-danger"
+                  onClick={() => handleRemoveItem(item)}
+                >
+                  Remove
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <div className={styles.checkoutActions}>
+        <Button variant="outline-secondary" onClick={handleReturnToProducts}>
+          Back to Products
+        </Button>
+        <Button variant="outline-success">Confirm Checkout</Button>{" "}
+        {/* Placeholder for checkout functionality */}
+      </div>
+    </div>
+  )
 }
 export default CheckoutPage
