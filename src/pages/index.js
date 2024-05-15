5fef23141102a16540c37c3839fa8c1c86125ef3
Aaron D. Tanner <sgtadt@gmail.com>
2024-05-15 11:49:01 -0500

    added conditional rendering based on context state to index.js. next will modify cart modal to be toggle-on interface.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,19 @@ import React from "react"
 import Layout from "../components/layout"
 import ErrorBoundary from "../components/ErrorBoundary"
 import ProductListing from "../components/ProductListing"
+import CheckoutPage from "../components/CheckoutPage"
+import { useAppContext } from "../components/AppContext"
 
 const ProductPage = () => {
+  // Read current state of checkout page visibility
+  const { isCheckoutVisible } = useAppContext()
+
   return (
     <>
       <Layout>
         <div>
           <ErrorBoundary>
-            <ProductListing />
+            {!isCheckoutVisible ? <ProductListing /> : <CheckoutPage />}
           </ErrorBoundary>
         </div>
       </Layout>
